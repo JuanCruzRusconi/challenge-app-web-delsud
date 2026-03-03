a8f023fa7ad98b1550df994dcb3c6674243576fe
JuanCruzRusconi <juanrusconi@hotmail.es>
2026-03-02 21:36:34 -0300

    refactor: layout.js, page.js, layout.module.css, page.module.css

--- challenge-app-web-delsud/src/app/layout.js
+++ challenge-app-web-delsud/src/app/layout.js
@@ -1,23 +1,33 @@
-import styles from "@/styles/globals.css";
 import NavBar from "@/components/layout/navBar/NavBar";
 import Footer from "@/components/layout/footer/Footer";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "@/styles/globals.css";
+import styles from "./layout.module.css";
+
+const plusJakartaFont = Plus_Jakarta_Sans({
+    subsets: ["latin"],
+    variable: "--font-primary",
+    weight: ["200","300","400","500","600","700"]
+});
 
 export default function RootLayout({ children }) {
   
-  return (
+    return (
+        
+        <html lang="es" className={plusJakartaFont.variable}>
+        
+            <body className={styles.layout_container}>
+            
+                <NavBar />
+                
+                <main className={styles.main_content}>
+                    {children}
+                </main>
+                
+                <Footer />
+        
+            </body>
+        </html>
     
-    <html lang="es">
-      
-      <body>
-        
-        <NavBar />
-        
-        {children}
-        
-        <Footer />
-      
-      </body>
-    </html>
-  
-    );
+        );
 }
--- challenge-app-web-delsud/src/app/page.js
+++ challenge-app-web-delsud/src/app/page.js
@@ -1,17 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import NavBar from "@/components/layout/navBar/NavBar";
-import Footer from "@/components/layout/footer/Footer";
-import PropertyHeader from "@/components/property/PropertyHeader";
-import PropertyDetails from "@/components/property/PropertyDetails";
+import PropertyDetail from "@/components/property/PropertyDetail";
+import PropertiesPage from "@/components/property/PropertiesPage";
+import Subtitle from "../components/layout/Subtitle";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <PropertyHeader />
-        <PropertyDetails />
-      </main>
-    </div>
+  
+    return (
+    
+        <main className={styles.main_container}>
+            <Subtitle text="Propiedades disponibles" />
+            <PropertiesPage />
+        </main>
+
   );
 }
