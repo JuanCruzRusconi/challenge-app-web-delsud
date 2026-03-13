de58bbfaa0c7726155150426499e76329f630254
JuanCruzRusconi <juanrusconi@hotmail.es>
2026-03-13 10:05:59 -0300

    feat: add generateStaticParams & static export for netlify deploy

--- challenge-app-web-delsud/src/app/properties/[id]/page.js
+++ challenge-app-web-delsud/src/app/properties/[id]/page.js
@@ -3,9 +3,19 @@ import PropertyDetail from "@/components/property/propertyDetail/PropertyDetail"
 import styles from "./page.module.css";
 import PropertiesCarroulsel from "@/components/property/PropertiesCarrousel";
 
+export const dynamicParams = false;
+
+export async function generateStaticParams () {
+    return properties
+    .filter((property) => property && property.id )
+    .map((property) => ({
+        id: String(property.id)
+    }))
+}
+
 export default async function Page({ params }) {
 
-    const { id } = await params;
+    const { id } = params;
 
     const property = properties.find(
         p => String(p.id) === id
